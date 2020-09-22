1ca3b63e9767ec5190f6095bb5d277a35e2fc7a5
Freet8707 <freet8707@gmail.com>
2020-09-21 21:34:45 -0500

    individual carID can be fetched from the db

--- routes/carsApi.js
+++ routes/carsApi.js
@@ -12,4 +12,16 @@ router.get('/',  async (req, res, next) => {
     }
 })
 
+router.get('/:id', async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const car = await db("cars-specs").where("carID", id).first();
+
+        res.status(200).json(car);
+    } catch(err) {
+        next(err)
+    }
+})
+
 module.exports = router
